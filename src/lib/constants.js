fef04016882eaa8a3c10e06552677d323e977155
Tuan Le <tuanleminh1235@gmail.com>
2019-02-02 18:54:00 -0500

    Add heroku server route

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 // url
-// export const ROUTE = "https://tandapay-chatserver.herokuapp.com"
-export const ROUTE = "http://134.53.166.46:4000"
+export const ROUTE = "https://zenio-server.herokuapp.com"
+// export const ROUTE = "http://134.53.166.46:4000"
 
 // action types
 export const SET_USER = "SET_USER"
