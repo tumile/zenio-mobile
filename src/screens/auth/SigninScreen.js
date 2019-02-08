7b592c806fdaa15f851e66ef2436397c21997c3b
Tuan Le <tuanleminh1235@gmail.com>
2019-02-07 23:24:22 -0500

    Fix view ios

--- src/screens/auth/SigninScreen.js
+++ src/screens/auth/SigninScreen.js
@@ -1,7 +1,7 @@
 import { EMAIL_NOT_FOUND, INVALID_EMAIL, WRONG_PASSWORD } from "lib/constants"
 import { authenticate } from "lib/redux/user/actions"
 import React, { Component } from "react"
-import { SafeAreaView } from "react-native"
+import { SafeAreaView, View } from "react-native"
 import { Button, HelperText, TextInput, Title } from "react-native-paper"
 import { connect } from "react-redux"
 import colors from "res/colors"
@@ -29,51 +29,57 @@ class SigninScreen extends Component {
             navigation: { navigate }
         } = this.props
         return (
-            <SafeAreaView
-                style={{ flex: 1, padding: 20, justifyContent: "center" }}>
-                <Title>Sign In</Title>
-                <HelperText
-                    style={{ fontSize: 14 }}
-                    type="error"
-                    visible={!!message}>
-                    {message}
-                </HelperText>
-                <TextInput
-                    style={{ backgroundColor: "transparent" }}
-                    label="Email"
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                    error={type === INVALID_EMAIL || type === EMAIL_NOT_FOUND}
-                    value={email}
-                    onChangeText={text => this.setState({ email: text })}
-                />
-                <TextInput
-                    style={{ backgroundColor: "transparent" }}
-                    label="Password"
-                    secureTextEntry={true}
-                    autoCapitalize="none"
-                    error={type === WRONG_PASSWORD}
-                    value={password}
-                    onChangeText={text => this.setState({ password: text })}
-                />
-                <Button
-                    style={{ marginVertical: 15 }}
-                    mode="contained"
-                    dark={true}
-                    uppercase={false}
-                    disabled={[email, password].some(i => i === "") || loading}
-                    loading={loading}
-                    onPress={this.handleAuth}>
-                    Sign In
-                </Button>
-                <Button
-                    style={{ marginTop: 15, borderColor: colors.primary }}
-                    mode="contained"
-                    dark={true}
-                    uppercase={false}
-                    onPress={() => navigate("Signup")}>
-                    Create Account
-                </Button>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View
+                    style={{ flex: 1, padding: 20, justifyContent: "center" }}>
+                    <Title>Sign In</Title>
+                    <HelperText
+                        style={{ fontSize: 14 }}
+                        type="error"
+                        visible={!!message}>
+                        {message}
+                    </HelperText>
+                    <TextInput
+                        style={{ backgroundColor: "transparent" }}
+                        label="Email"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                        error={
+                            type === INVALID_EMAIL || type === EMAIL_NOT_FOUND
+                        }
+                        value={email}
+                        onChangeText={text => this.setState({ email: text })}
+                    />
+                    <TextInput
+                        style={{ backgroundColor: "transparent" }}
+                        label="Password"
+                        secureTextEntry={true}
+                        autoCapitalize="none"
+                        error={type === WRONG_PASSWORD}
+                        value={password}
+                        onChangeText={text => this.setState({ password: text })}
+                    />
+                    <Button
+                        style={{ marginVertical: 15 }}
+                        mode="contained"
+                        dark={true}
+                        uppercase={false}
+                        disabled={
+                            [email, password].some(i => i === "") || loading
+                        }
+                        loading={loading}
+                        onPress={this.handleAuth}>
+                        Sign In
+                    </Button>
+                    <Button
+                        style={{ marginTop: 15, borderColor: colors.primary }}
+                        mode="contained"
+                        dark={true}
+                        uppercase={false}
+                        onPress={() => navigate("Signup")}>
+                        Create Account
+                    </Button>
+                </View>
             </SafeAreaView>
         )
     }
--- src/screens/auth/SignupScreen.js
+++ src/screens/auth/SignupScreen.js
@@ -34,72 +34,74 @@ class SignupScreen extends React.Component {
             error: { type, message }
         } = this.props
         return (
-            <SafeAreaView
-                style={{ flex: 1, padding: 20, justifyContent: "center" }}>
-                <Title>Create Account</Title>
-                <HelperText
-                    style={{ fontSize: 14 }}
-                    type="error"
-                    visible={!!message}>
-                    {message}
-                </HelperText>
-                <View style={{ flexDirection: "row" }}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View
+                    style={{ flex: 1, padding: 20, justifyContent: "center" }}>
+                    <Title>Create Account</Title>
+                    <HelperText
+                        style={{ fontSize: 14 }}
+                        type="error"
+                        visible={!!message}>
+                        {message}
+                    </HelperText>
+                    <View style={{ flexDirection: "row" }}>
+                        <TextInput
+                            style={{
+                                backgroundColor: "transparent",
+                                flex: 1,
+                                marginRight: 2.5
+                            }}
+                            label="First Name"
+                            value={givenName}
+                            onChangeText={text =>
+                                this.setState({ givenName: text })
+                            }
+                        />
+                        <TextInput
+                            style={{
+                                backgroundColor: "transparent",
+                                flex: 1,
+                                marginLeft: 2.5
+                            }}
+                            label="Last Name"
+                            value={familyName}
+                            onChangeText={text =>
+                                this.setState({ familyName: text })
+                            }
+                        />
+                    </View>
                     <TextInput
-                        style={{
-                            backgroundColor: "transparent",
-                            flex: 1,
-                            marginRight: 2.5
-                        }}
-                        label="First Name"
-                        value={givenName}
-                        onChangeText={text =>
-                            this.setState({ givenName: text })
-                        }
+                        style={{ backgroundColor: "transparent" }}
+                        label="Email"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                        error={type === INVALID_EMAIL || type === EMAIL_TAKEN}
+                        value={email}
+                        onChangeText={text => this.setState({ email: text })}
                     />
                     <TextInput
-                        style={{
-                            backgroundColor: "transparent",
-                            flex: 1,
-                            marginLeft: 2.5
-                        }}
-                        label="Last Name"
-                        value={familyName}
-                        onChangeText={text =>
-                            this.setState({ familyName: text })
-                        }
+                        style={{ backgroundColor: "transparent" }}
+                        label="Password"
+                        secureTextEntry={true}
+                        autoCapitalize="none"
+                        value={password}
+                        onChangeText={text => this.setState({ password: text })}
                     />
+                    <Button
+                        style={{ marginVertical: 15 }}
+                        mode="contained"
+                        dark={true}
+                        uppercase={false}
+                        disabled={
+                            [givenName, familyName, email, password].some(
+                                i => i === ""
+                            ) || loading
+                        }
+                        loading={loading}
+                        onPress={this.handleAuth}>
+                        Sign Up
+                    </Button>
                 </View>
-                <TextInput
-                    style={{ backgroundColor: "transparent" }}
-                    label="Email"
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                    error={type === INVALID_EMAIL || type === EMAIL_TAKEN}
-                    value={email}
-                    onChangeText={text => this.setState({ email: text })}
-                />
-                <TextInput
-                    style={{ backgroundColor: "transparent" }}
-                    label="Password"
-                    secureTextEntry={true}
-                    autoCapitalize="none"
-                    value={password}
-                    onChangeText={text => this.setState({ password: text })}
-                />
-                <Button
-                    style={{ marginVertical: 15 }}
-                    mode="contained"
-                    dark={true}
-                    uppercase={false}
-                    disabled={
-                        [givenName, familyName, email, password].some(
-                            i => i === ""
-                        ) || loading
-                    }
-                    loading={loading}
-                    onPress={this.handleAuth}>
-                    Sign Up
-                </Button>
             </SafeAreaView>
         )
     }
